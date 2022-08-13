b46c7220302e4d0ac7b8c561045c921ec8106f01
Abhiram Reddy <abhiram@Abhirams-MacBook-Air.local>
2022-08-14 02:50:08 +0530

    Add test cases for Line

--- lib/line.rb
+++ lib/line.rb
@@ -1,5 +1,14 @@
 module Geometry
   class Line
-
+    def initialize(point_1, point_2)
+      if !(point_2.is_a?(Point) && point_1.is_a?(Point))
+        raise("objects passed to line must be point objects")
+      end
+      if point_1.x == point_2.x && point_1.y == point_2.y
+        raise("both points are the same")
+      end
+      @point_1 = point_1
+      @point_2 = point_2
+    end
   end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -1,5 +1,16 @@
 require "line"
+require "line/point"
 
 RSpec.describe Geometry::Line do
-
+  context "Line Initialisation" do
+    it "should check id the Line object is created" do
+      expect { Geometry::Line.new(Geometry::Point.new(1, 2), Geometry::Point.new(4, 6)) }.to_not raise_error
+    end
+    it "should raise error when point objects are not passed" do
+      expect { Geometry::Line.new(Geometry::Point.new(1, 2), "p") }.to raise_error("objects passed to line must be point objects")
+    end
+    it "should not create a line when points are the same" do
+      expect { Geometry::Line.new(Geometry::Point.new(1, 2), Geometry::Point.new(1, 2)) }.to raise_error("both points are the same")
+    end
+  end
 end
